2378994adfa97e643f27f712dc9c57dd335095b9
Santiago Acuña <snt-26@hotmail.com>
2022-08-09 18:03:56 -0500

    formating double valor compra

--- Server_Notificaciones/src/main/java/servidor/controladores/ControladorNotificacionesImp.java
+++ Server_Notificaciones/src/main/java/servidor/controladores/ControladorNotificacionesImp.java
@@ -11,8 +11,8 @@ public class ControladorNotificacionesImp extends ControladorNotificacionesIntPO
         System.out.println("-------Nueva notificacion---------");
         System.out.println("Id: "+objNotificacion.Id);
         System.out.println("Tipo de Identificacion: "+objNotificacion.TipoIdentificacion);
-        System.out.printf("%nValorCompra: %,2f",objNotificacion.ValorCompra);
-        System.out.println("Lugar de Compra: "+objNotificacion.LugarCompra);
+        System.out.printf("ValorCompra: %,.2f",objNotificacion.ValorCompra);
+        System.out.println("\nLugar de Compra: "+objNotificacion.LugarCompra);
         System.out.println("Fecha y Hora de Compra: "+objNotificacion.FechaHora);
         System.out.println("Medio de pago: "+objNotificacion.MedioPago);
         System.out.println("Nit de Empresa: "+objNotificacion.NitEmpresa);
